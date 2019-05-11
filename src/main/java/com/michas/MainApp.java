3fa70fd7da3c0e7362c77be3f41e478dc66e377f
MichasC <m.scibek@outlook.com>
2019-05-11 12:08:43 +0200

    Edited method in MainApp class.

--- src/main/java/com/michas/MainApp.java
+++ src/main/java/com/michas/MainApp.java
@@ -15,7 +15,7 @@ public class MainApp extends Application {
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
-        stage.setTitle("Projekt 1 - wielowątkowość");
+        stage.setTitle("Multithreaded");
         stage.setScene(scene);
         stage.show();
     }
